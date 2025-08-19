a10a6072f6544a84a54baaf2e9206ef4468acefe
Adrenaline189 <luminexenergyglobal@gmail.com>
2025-08-19 22:32:39 +0700

    Delete components directory

--- components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-export function Card({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`bg-slate-900/60 border border-slate-800 rounded-2xl ${className}`} {...props} />;
-}
-export function CardContent({ className = "", ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={`p-5 ${className}`} {...props} />;
-}
