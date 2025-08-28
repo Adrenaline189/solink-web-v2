63adddc24a6c8972ab44f91441401b8b5740f41b
Adrenaline189 <luminexenergyglobal@gmail.com>
2025-08-29 00:06:20 +0700

    Delete app directory

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-// app/loading.tsx
-export default function RootLoading() {
-  return (
-    <div className="min-h-[50vh] flex items-center justify-center p-6">
-      <div className="animate-pulse text-slate-400">Loading…</div>
-    </div>
-  );
-}
