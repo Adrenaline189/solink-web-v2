80fda1c4e49f97ac33269d23f9819d8cd538ca7f
Adrenaline189 <luminexenergyglobal@gmail.com>
2025-11-07 18:30:37 +0700

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
 // app/dashboard/page.tsx
 "use client";
+import dynamic from "next/dynamic";
+const WalletMultiButton = dynamic(
+  async () => (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+  { ssr: false }
+);
 
 import { Suspense, useEffect, useState, useCallback } from "react";
 export const dynamic = "force-dynamic";
